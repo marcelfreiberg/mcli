833f205239eab781bba8ce8424ef56ea3e1c7077
Marcel Freiberg <marcel.freiberg@outlook.com>
2023-02-08 14:31:29 +0100

    Execute the script based on the file extension

--- marcellocli.py
+++ marcellocli.py
@@ -4,35 +4,66 @@ import os
 from simple_term_menu import TerminalMenu
 
 
-def list_files(directory):
+def list_files(directory) -> list:
+    """List all files in a directory
+
+    Parameters
+    ----------
+    directory : str
+        The directory to list files from
+
+    Returns
+    -------
+    list
+        A list of files in the directory as a dict
+    """
+
     files = os.listdir(directory)
     files = [file for file in files if os.path.isfile(
         os.path.join(directory, file))]
-    return files
+
+    table = []
+    for file in files:
+        file_name = os.path.splitext(file)[0]
+        file_extension = os.path.splitext(file)[1]
+        absolute_path = os.path.abspath(directory)
+        file_path = os.path.join(absolute_path, file)
+
+        table.append({'file': file,
+                      'file_name': file_name,
+                      'file_extension': file_extension,
+                      'absolute_path': absolute_path,
+                      'file_path': file_path})
+    return table
 
 
-def cut_extensions(files):
-    files = [file.split(".")[0] for file in files]
-    return files
+def execute_script(file):
+    """Execute a script based on the file extension
 
-
-def execute_script(script):
-    os.system("python3 ./scripts/" + script + ".py")
-
-
-def execute_bash_script(script):
-    os.system("./scripts/" + script + ".sh")
+    Parameters
+    ----------
+    file : dict
+        A dict containing the file information
+    """
+    match file['file_extension']:
+        case ".py":
+            os.system("python3" + file['file_path'])
+        case ".sh":
+            os.system(file['file_path'])
+        case _:
+            print("File extension not supported")
 
 
 def main():
-    options = list_files("./scripts")
-    options = cut_extensions(options)
+    files = list_files("./scripts")
+    options = [d['file_name'] for d in files]
 
     terminal_menu = TerminalMenu(options)
     menu_entry_index = terminal_menu.show()
 
     print("You selected: " + options[menu_entry_index])
-    execute_bash_script(options[menu_entry_index])
+
+    execute_script(files[menu_entry_index])
 
 
 if __name__ == "__main__":
--- scripts/test.sh
+++ scripts/test.sh
@@ -5,7 +5,7 @@ MINIMUM_NODE_VERSION='12.0.0'
 CURRENT_NODE_VERSION=$(node -v)
 CURRENT_NODE_VERSION=${CURRENT_NODE_VERSION:1}
 
-if [ "$CURRENT_NODE_VERSION" /> "$MINIMUM_NODE_VERSION" ]; then
+if [ "$CURRENT_NODE_VERSION" -ge "$MINIMUM_NODE_VERSION" ]; then
   :
 else
   echo "Error: Current version of Node.js ($CURRENT_NODE_VERSION) does not fulfill the minimum required version $MINIMUM_NODE_VERSION"
