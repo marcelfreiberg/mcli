950e1a65b0cb01a99e2e0fa86f4693462e51b734
Marcel Freiberg <marcel.freiberg@outlook.com>
2023-08-06 13:23:14 +0200

    Add string formatters

--- install.sh
+++ install.sh
@@ -7,6 +7,20 @@ abort() {
   exit 1
 }
 
+# string formatters
+if [[ -t 1 ]]
+then
+  tty_escape() { printf "\033[%sm" "$1"; }
+else
+  tty_escape() { :; }
+fi
+tty_mkbold() { tty_escape "1;$1"; }
+tty_underline="$(tty_escape "4;39")"
+tty_blue="$(tty_mkbold 34)"
+tty_red="$(tty_mkbold 31)"
+tty_bold="$(tty_mkbold 39)"
+tty_reset="$(tty_escape 0)"
+
 shell_join() {
   local arg
   printf "%s" "$1"
